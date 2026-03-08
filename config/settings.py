f30ca47d78bc13178d198cb71b254e5d78d345ba
o1kku <oliver.ekman@live.fi>
2026-03-08 17:59:11 +0200

    pin configs updated

--- config/settings.py
+++ config/settings.py
@@ -4,19 +4,19 @@ Contains GPIO pin mappings and default operational parameters.
 """
 
 # Stepper Motors (ULN2003 uses 4 pins each)
-STEPPER_HEIGHT_PINS = (6, 13, 19, 26)
-STEPPER_DISTANCE_PINS = (12, 16, 20, 21)
+STEPPER_HEIGHT_PINS = (7, 1, 11, 0)
+STEPPER_DISTANCE_PINS = (5, 6, 19, 26)
 
 # Servo Motor
-SERVO_PIN = 18
+SERVO_PIN = 13
 
 # Limit Switches
-SWITCH_X_PIN = 17
-SWITCH_Y_PIN = 27
-SWITCH_Z_PIN = 22
+SWITCH_ROTATE_PIN = 16
+SWITCH_DISTANCE_PIN = 20
+SWITCH_HEIGHT_PIN = 21
 
 # Electromagnet
-MAGNET_PIN = 23
+MAGNET_PIN = 12
 
 # Operational Defaults
 DEFAULT_STEP_DELAY = 0.002
