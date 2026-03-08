82d14a2d1149a2bf28fd6b78a56e129fde94956f
o1kku <oliver.ekman@live.fi>
2026-03-08 17:59:44 +0200

    code for motor testing

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ This program provides a main interface for
 controlling the ChessRobot on Raspberry Pi 5. 
 """
 
+import time
+
+from config.settings import STEPPER_DISTANCE_PINS
+from src.hardware.stepper import StepMotor
+
 def main():
-    return
+    dist_motor = StepMotor(STEPPER_DISTANCE_PINS)
+
+    dist_motor.step(500)
+    time.delay(1)
+    dist_motor.step(500, -1)
 
 if __name__=="__main__":
     main()
