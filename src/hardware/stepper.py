91a2e5292158f989b09de971db3d50425487b7ea
o1kku <oliver.ekman@live.fi>
2026-04-03 20:47:20 +0300

    Implemented method that stops the motor when called.

--- src/hardware/stepper.py
+++ src/hardware/stepper.py
@@ -7,9 +7,9 @@ import time
 try:
     from gpiozero import OutputDevice
 except ImportError:
-    print("Warning: gpiozero is not available. Entering mock-mode.")
+    print("Warning: gpiozero is not available. Stepper motor entering mock-mode.")
     class OutputDevice:
-        def __init__(self): self.pin = pin; self.value = 0
+        def __init__(self, pin): self.pin = pin; self.value = 0
         def __repr__(self): return f"Pin({self.pin})"
 
 class StepMotor:
@@ -19,7 +19,7 @@ class StepMotor:
 
     Attributes:
         pins (list): A list of GPIO pins (IN1, IN2, IN3, IN4).
-        delay (float): The delay between steps in seconds.
+        delay_sec (float): The delay between steps in seconds.
     """
 
     # Half-step sequence for 28BYJ-48 motor.
@@ -30,36 +30,54 @@ class StepMotor:
         [0, 0, 0, 1], [1, 0, 0, 1]
     ]
 
-    def __init__(self, pins, delay=0.002):
+    def __init__(self, pins, delay_sec=0.002):
         """
         Initialize the stepper motor.
+
+        :param pins: A list of GPIO pins (IN1, IN2, IN3, IN4).
+        :param delay_sec: The delay between steps in seconds.
         """
-        self.delay = delay
+        self.delay_sec = delay_sec
         self.pins = [OutputDevice(pin) for pin in pins]
+        self.running = False
+
+    def stop(self):
+        """
+        Stops the motor movement immediately and clears pins.
+        """
+        self._running = False
+        for pin in self.pins:
+            pin.value = 0
 
     def _make_step(self, sequence):
         """
-        Private method to make the motor do one step.
+        Make the motor do one full sequence cycle.
 
-        Args:
-            sequence (list): _STEP_SEQUENCE in correct order
+        :param sequence: _STEP_SEQUENCE in correct order
         """
         for step in sequence:
+            # Check if stop() is called.
+            if not self._running:
+                return
             for pin, value in zip(self.pins, step):
                 pin.value = value
-                time.sleep(self.delay)
+                time.sleep(self.delay_sec)
         
     def step(self, steps_nbr, direction=1):
         """
         Moves the motor by a pecified number of steps.
         
-        Args:
-            steps (int): The number of steps to move.
-            direction (int): 1 for closer, -1 for further.
+        :param steps: The number of steps to move.
+        :param direction: 1 for clockwise, -1 for counterclockwise.
         """
+        self._running = True
         # Direction
         sequence = self._STEP_SEQUENCE if direction == 1 else self._STEP_SEQUENCE[::-1]
 
         # Make a step steps_nbr times in correct direction
         for _ in range(steps_nbr):
+            if not self._running:
+                break
             self._make_step(sequence)
+        
+        self._running = False
